cd6d5d7b118a7d81efdf716fe4492543a4a8ded1
Ivo Maixner <ivo.maixner@gmail.com>
2013-09-27 16:47:37 +0200

    Add test curl command.

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -v -H "Content-type: application/json" -d\
+ '{"action":"greetingService","method":"sayHello","data":{"user":{"firstName":"aa","lastName":"bb"}},"type":"rpc","tid":1}'\
+ http://localhost:8080/ext-direct-sample/action/router
